fef888c4c585b4ebdc4f37d28624544140cf92eb
Banetta <banetion@gmail.com>
2018-03-31 12:21:56 +0900

    180331 HomeWork Editing

--- source/tr/c-tr-005.c
+++ source/tr/c-tr-005.c
@@ -7,18 +7,18 @@
 // 홍길동
 
 typedef struct Student{
-	char school[10];
-	char dept[10];
-	char name[10];
+	char school[20];
+	char dept[20];
+	char name[20];
 } student;
 
 // 제시된 소스 1
 // Return by return type of struct *
-char *function1(student *st);
-char *function2(student *st);
-char *function3(student *st);
+char *function1(char *school);
+char *function2(char *dept);
+char *function3(char *name);
 // 소스 1 끝
-student *memory(student *st);
+//student *memory(student *st);
 
 int main(){
 	
@@ -33,37 +33,44 @@ int main(){
 	char *school = NULL;
 	char *dept = NULL;
 	char *name = NULL;
-
-	school = function1(st);
-	dept = function2(st);
-	name = function3(st);
-	printf("%s\n%s\n%s\n", school, dept, name);
-	return 0;
-	// 소스 2 끝
-}
-
-student *memory(student *st){
-
+	
 	st = malloc(sizeof(struct Student));
 	if ( st == NULL ){
 		fprintf(stderr, "Improper value of st\n");
 		exit (1);
 	}
 
-	return st;
+
+	school = function1(st->school);
+	dept = function2(st->dept);
+	name = function3(st->name);
+	printf("%s\n%s\n%s\n", school, dept, name);
+	return 0;
+	// 소스 2 끝
 }
 
-char *function1(student *st){
-	strcpy(st->school, "한국산업기술대학교");
-	return st->school;
+//student *memory(student *st){
+//
+//	st = malloc(sizeof(struct Student));
+//	if ( st == NULL ){
+//		fprintf(stderr, "Improper value of st\n");
+//		exit (1);
+//	}
+//
+//	return st;
+//}
+
+char *function1(char *school){
+	strcpy(school, "한국산업기술대학교");
+	return school;
 }
 
-char *function2(student *st){
-	strcpy(st->dept, "컴퓨터공학부");
-	return st->dept;
+char *function2(char *dept){
+	strcpy(dept, "컴퓨터공학부");
+	return dept;
 }
 
-char *function3(student *st){
-	strcpy(st->name, "홍길동");
-	return st->name;
+char *function3(char *name){
+	strcpy(name, "홍길동");
+	return name;
 }
